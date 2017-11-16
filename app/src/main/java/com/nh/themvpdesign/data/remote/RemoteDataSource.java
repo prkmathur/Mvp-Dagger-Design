248d92bc14a04debfb5676da0d211b0cc17dca43
prakhar <ashu.mantri@dotsquares.com>
2017-11-16 18:04:43 +0530

    DS changes

--- app/src/main/java/com/nh/themvpdesign/data/remote/RemoteDataSource.java
+++ app/src/main/java/com/nh/themvpdesign/data/remote/RemoteDataSource.java
@@ -4,8 +4,16 @@ import android.content.SharedPreferences;
 
 import com.nh.themvpdesign.data.DataSource;
 import com.nh.themvpdesign.data.NetworkService;
+import com.nh.themvpdesign.models.GithubRepo;
+
+import java.util.List;
 
 import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
 /**
  * Created by dell pc on 12-11-2017.
@@ -24,9 +32,22 @@ public class RemoteDataSource implements DataSource {
     }
 
     @Override
-    public void getGithubRepos(String username, getgithubRepo callback) {
+    public void getGithubRepos(String username, final getgithubRepo callback) {
 
-        
+        Call<List<GithubRepo>> listCall =  networkService.getReposForUser(username);
+        listCall.enqueue(new Callback<List<GithubRepo>>() {
+            @Override
+            public void onResponse(Call<List<GithubRepo>> call, Response<List<GithubRepo>> response) {
+                Timber.i(response.body().toString());
+                callback.githubRepoResponse(response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<GithubRepo>> call, Throwable t) {
+                Timber.i(t.toString());
+                callback.networkError(t.toString());
+            }
+        });
 
     }
 }
